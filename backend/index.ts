62414128aa54ca65d22f0f23daa4ff745d0e724d
Jozeene Bailey <jozeenebailey@Jozeenes-MBP.home>
2020-11-27 10:36:23 +0000

    port

--- backend/index.ts
+++ backend/index.ts
@@ -5,7 +5,7 @@ import cors from "cors";
 import routes from "./routes/blogRoutes";
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 //mongo connection
 mongoose.Promise = global.Promise;
