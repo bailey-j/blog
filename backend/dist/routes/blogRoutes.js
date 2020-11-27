4579827ba3fe49dbd26f3ab01e367a7167d3dd22
Jozeene Bailey <jozeenebailey@Jozeenes-MBP.home>
2020-11-27 11:48:12 +0000

    restructure 1

--- backend/dist/routes/blogRoutes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const postControllers_1 = require("../controllers/postControllers");
-const routes = (app) => {
-    app.route("/posts").get(postControllers_1.getPosts).post(postControllers_1.addNewPost);
-    app.route("/post/:postId").get(postControllers_1.getPostById).put(postControllers_1.updatePost).delete(postControllers_1.deletePost);
-};
-exports.default = routes;
